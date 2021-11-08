fb90528e8b204c9530be0976ed5055ce7940211c
joshNic <256jomu@gmail.com>
2021-11-07 19:33:34 -0600

    add maze

---
+++ maze/maze.js
@@ -0,0 +1,32 @@
+$(document).ready(()=> {
+    $("#start").click(reset);
+    $(".boundary").mouseover(red);
+    $("#end").mouseover(end);
+});
+
+let isWallHit = false;
+let isStarted = false;
+let reset = () => {
+    isWallHit = false;
+    isStarted = true;
+    $(".boundary").removeClass("youlose");
+    $("#status").text('Click the "S" to begin. ');
+}
+
+let red = () => {
+    if (isStarted) {
+        isWallHit = true;
+        $(".boundary").addClass("youlose");
+    }
+}
+
+let end = () => {
+    if (isStarted) {
+        if (!isWallHit) {
+            $("#status").text("You win! :]");
+        } else {
+            $("#status").text("You lost! :[");
+        }
+    }
+    isStarted = false;
+}
